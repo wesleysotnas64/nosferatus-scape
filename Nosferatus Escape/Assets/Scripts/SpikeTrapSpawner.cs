099427a0a07127fc721297eb3f8d249a31f97b51
wesleysotnas64 <wesleysotnas64@gmail.com>
2025-01-30 17:33:04 -0300

    feat: Instantiate Right To Left Spikes.

--- Nosferatus Escape/Assets/Scripts/SpikeTrapSpawner.cs
+++ Nosferatus Escape/Assets/Scripts/SpikeTrapSpawner.cs
@@ -30,6 +30,7 @@ public class SpikeTrapSpawner : MonoBehaviour
         // }
 
         if (Input.GetKeyDown(KeyCode.P)) StartCoroutine(InstantiateLeftToRightSpikesCoroutine(0.5f));
+        if (Input.GetKeyDown(KeyCode.O)) StartCoroutine(InstantiateRightToLeftSpikesCoroutine(0.5f));
     }
 
     private void InstantitateSpikeTrap()
@@ -50,6 +51,24 @@ public class SpikeTrapSpawner : MonoBehaviour
         spikeTrapParam *= _velocity;
 
         for(int i = 0; i < xPosition.Count; i++)
+        {
+            GameObject spikeTrapGO = Instantiate(spikeTrap);
+            spikeTrapGO.transform.position = new Vector3(xPosition[i], transform.position.y, 0);
+            StartCoroutine(
+                spikeTrapGO.GetComponent<SpikeTrap>().ActiveCoroutine(
+                    spikeTrapParam.x, spikeTrapParam.y, spikeTrapParam.z));
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+
+    private IEnumerator InstantiateRightToLeftSpikesCoroutine(float _velocity = 1.0f)
+    {
+        List<float> xPosition = new() { -0.8f, -0.6f, -0.4f, -0.2f, 0.0f, 0.2f, 0.4f, 0.6f, 0.8f };
+        float waitTime = 0.5f * _velocity;
+        Vector3 spikeTrapParam = new(0.25f, 0.5f, 0.25f);
+        spikeTrapParam *= _velocity;
+
+        for(int i = xPosition.Count-1; i >= 0; i--)
         {
             GameObject spikeTrapGO = Instantiate(spikeTrap);
             spikeTrapGO.transform.position = new Vector3(xPosition[i], transform.position.y, 0);
